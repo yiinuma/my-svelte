b168859605a9c5bbb17cb87c8168ac08828f4b77
yiinuma <65111001+yiinuma@users.noreply.github.com>
2022-12-04 21:24:51 +0900

    tailwindcss適用 & Users実装

---
+++ src/lib/Users.svelte
@@ -0,0 +1,42 @@
+<script>
+  let users = [
+    {
+      id: 1,
+      image: 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png',
+      name: 'John',
+      userEmail: 'John@bitfumes.com',
+    },
+    {
+      id: 2,
+      image: 'https://cdn-icons-png.flaticon.com/512/201/201634.png',
+      name: 'Jenie',
+      userEmail: 'Jenie@bitfumes.com',
+    },
+    {
+      id: 3,
+      image: 'https://cdn-icons-png.flaticon.com/512/560/560277.png',
+      name: 'Keiji',
+      userEmail: 'Keiji@bitfumes.com',
+    },
+  ]
+
+  const getUsers = () => {
+    return users
+  }
+</script>
+
+<div>
+  <h1 class="text-2xl text-center mt-10">List of Users</h1>
+  {#each getUsers() as { image, name, userEmail, id }, i (id)}
+    <div
+      class="flex m-5 bg-white border p-2 rounded-lg shadow-md hover:shadow-lg"
+    >
+      <p class="w-10">{i + 1}</p>
+      <img class="rounded-full w-14 h-14" src={image} alt={name} />
+      <h3 class="mx-4 w-20">{name}</h3>
+      <p class="mx-4 w-20">{userEmail}</p>
+    </div>
+  {:else}
+    <p>No user found</p>
+  {/each}
+</div>
